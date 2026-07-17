067ffb94201c4a5ba0132efa938192b3217e36c6
hasnaainali <i.hasnainali.005@gmail.com>
2026-07-17 11:25:45 +0500

    Add components export file

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Navbar } from "./logic/Navbar";
+export { default as Hero } from "./logic/Hero";
+export { default as About } from "./logic/About";
+export { default as Skills } from "./logic/Skills";
+export { default as Services } from "./logic/Services";
+export { default as Projects } from "./logic/Projects";
+export { default as Contact } from "./logic/Contact";
